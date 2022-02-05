136e2c15a0de1f0794e4d6ab34a9e3f02b38c0a0
Kartik Verma <97230777+KartikVerma0@users.noreply.github.com>
2022-02-06 02:50:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,51 @@
-const playerOneScore = document.querySelector("#playerOneScore");
-const playerTwoScore = document.querySelector("#playerTwoScore");
-const playerOneScoreBtn = document.querySelector("#playerOneScoreBtn");
-const playerTwoScoreBtn = document.querySelector("#playerTwoScoreBtn");
+const players = [
+    {
+        score: document.querySelector("#playerOneScore"),
+        scoreBtn: document.querySelector("#playerOneScoreBtn"),
+    },
+    {
+        score: document.querySelector("#playerTwoScore"),
+        scoreBtn: document.querySelector("#playerTwoScoreBtn"),
+    },
+];
+
+const totalPlayers = players.length;
 const resetBtn = document.querySelector("#resetBtn");
 const maxScore = document.querySelector("#maxScore");
-let maxScoreValue = maxScore.value;
 
-maxScore.addEventListener("click", function () {
-    maxScoreValue = maxScore.value;
-});
-
-playerOneScoreBtn.addEventListener("click", () => {
-    playerOneScore.textContent = parseInt(playerOneScore.textContent) + 1;
-    maxScoreValue = maxScore.value;
-    if (playerOneScore.textContent == maxScoreValue) {
-        playerOneScore.classList.add("has-text-success");
-        playerTwoScore.classList.add("has-text-danger");
-        playerOneScoreBtn.setAttribute("disabled", "true");
-        playerTwoScoreBtn.setAttribute("disabled", "true");
+function updateScore(players, playingOrder) {
+    let playerIndex = playingOrder - 1;
+    let maxScoreValue = maxScore.value;
+    players[playerIndex].score.textContent =
+        parseInt(players[playerIndex].score.textContent) + 1;
+    if (players[playerIndex].score.textContent == maxScoreValue) {
+        for (let i = 0; i < totalPlayers; i++) {
+            players[i].scoreBtn.setAttribute("disabled", "true");
+            if (i === playerIndex) {
+                players[i].score.classList.add("has-text-success");
+            } else {
+                players[i].score.classList.add("has-text-danger");
+            }
+        }
     }
-});
+}
 
-playerTwoScoreBtn.addEventListener("click", () => {
-    playerTwoScore.textContent = parseInt(playerTwoScore.textContent) + 1;
-    maxScoreValue = maxScore.value;
-    if (playerTwoScore.textContent == maxScoreValue) {
-        playerTwoScore.classList.add("has-text-success");
-        playerOneScore.classList.add("has-text-danger");
-        playerOneScoreBtn.setAttribute("disabled", "true");
-        playerTwoScoreBtn.setAttribute("disabled", "true");
+function reset(players) {
+    for (let i = 0; i < totalPlayers; i++) {
+        players[i].score.textContent = 0;
+        players[i].score.classList.remove("has-text-success");
+        players[i].score.classList.remove("has-text-danger");
+        players[i].scoreBtn.removeAttribute("disabled");
     }
-});
+}
+
+for (let i = 0; i < totalPlayers; i++) {
+    players[i].scoreBtn.addEventListener("click", () => {
+        updateScore(players, i + 1);
+    });
+}
 
 resetBtn.addEventListener("click", () => {
     maxScoreValue = maxScore.value;
-    playerOneScore.textContent = 0;
-    playerTwoScore.textContent = 0;
-    playerOneScore.classList.remove("has-text-success");
-    playerTwoScore.classList.remove("has-text-danger");
-    playerTwoScore.classList.remove("has-text-success");
-    playerOneScore.classList.remove("has-text-danger");
-    playerOneScoreBtn.removeAttribute("disabled");
-    playerTwoScoreBtn.removeAttribute("disabled");
+    reset(players);
 });
